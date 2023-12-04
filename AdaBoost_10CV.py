a25d7315438e9c1adfa967783d1fd1592411fd76
Prospero1988 <134698207+Prospero1988@users.noreply.github.com>
2023-12-04 14:12:08 +0100

    Final scripts update

---
+++ AdaBoost_10CV.py
@@ -0,0 +1,128 @@
+import pandas as pd
+from sklearn.model_selection import KFold
+from sklearn.ensemble import AdaBoostRegressor  # Zmiana importu na AdaBoostRegressor
+from sklearn.metrics import mean_squared_error
+import math
+import os
+import time
+import numpy as np
+from multiprocessing import Pool, cpu_count
+
+# Clear the terminal screen
+# os.system('cls' if os.name == 'nt' else 'clear')
+
+# Pobierz nazwę skryptu bez rozszerzenia
+script_name = os.path.splitext(os.path.basename(__file__))[0]
+
+# Read the CSV file containing the list of files
+print()
+list_csv_input = input("Enter the names of CSV files with lists of files (comma-separated): ")
+list_csv = list_csv_input.split(',')
+
+# Start measuring the execution time
+total_execution_start_time = time.time()
+
+# Function to process a single CSV file
+def process_csv(csv_file):
+    try:
+        # Read the current CSV file
+        file_list = pd.read_csv(csv_file.strip())
+        output_file_name = f"{script_name}_{os.path.splitext(csv_file.strip())[0].replace('.', '_')}_output.csv"  # Change the output file name
+
+        # Create an empty list to store RMSE values and standard deviations for each fold
+        rmse_values = []
+        std_dev_values = []
+
+        # Start measuring the execution time for the current CSV file
+        start_time = time.time()
+
+        # Iterate over the files
+        for file_name in file_list['File Path']:
+            try:
+                # Read the current file
+                data = pd.read_csv(file_name)
+
+                # Extract the features and target variable
+                X = data.iloc[:, 1:]  # Features (assuming they start from the second column)
+                y = data.iloc[:, 0]  # Target variable (first column)
+
+                # Check if there are enough data points for training
+                if len(data) <= 1:
+                    raise ValueError("Insufficient data points in file: {}".format(file_name))
+
+                # Create an AdaBoost Regressor model
+                adaboost_regressor = AdaBoostRegressor()  # Utwórz obiekt AdaBoostRegressor
+
+                # Perform 10-fold cross-validation
+                kf = KFold(n_splits=10, shuffle=True, random_state=42)
+                fold_rmse_values = []
+
+                for train_index, test_index in kf.split(X):
+                    X_train, X_test = X.iloc[train_index], X.iloc[test_index]
+                    y_train, y_test = y.iloc[train_index], y.iloc[test_index]
+
+                    # Train the model
+                    adaboost_regressor.fit(X_train, y_train)
+
+                    # Make predictions on the test set
+                    y_pred = adaboost_regressor.predict(X_test)
+
+                    # Calculate the mean squared error of the model
+                    mse = mean_squared_error(y_test, y_pred)
+
+                    # Calculate the root mean squared error (RMSE)
+                    rmse = math.sqrt(mse)
+
+                    # Store RMSE value for this fold
+                    fold_rmse_values.append(rmse)
+
+                # Calculate Averaged RMSE and Standard Deviation for this file
+                averaged_rmse = np.mean(fold_rmse_values)
+                std_dev_rmse = np.std(fold_rmse_values)
+
+                # Store the averaged RMSE and Standard Deviation values in the lists
+                rmse_values.append(averaged_rmse)
+                std_dev_values.append(std_dev_rmse)
+
+            except Exception as e:
+                print("Error processing file: {}. {}".format(file_name, str(e)))
+
+        # Stop measuring the execution time for the current CSV file
+        end_time = time.time()
+
+        # Calculate the total execution time for the current CSV file
+        execution_time = end_time - start_time
+
+        # Print the file names with corresponding Averaged RMSE values and standard deviations
+        print()
+        print(" Averaged RMSE values with Standard Deviations for datasets in {}: ".format(csv_file.strip()))
+        print()
+        for file_name, rmse, std_dev in zip(file_list['File Path'], rmse_values, std_dev_values):
+            # Extract the filename from the file path
+            filename = os.path.basename(file_name)
+            print("Averaged RMSE: {:.5f}  ----->  Standard Deviation of RMSE: {:.5f} ----->  File: {}".format(rmse, std_dev, filename))
+        print("Execution time for {}: {:.2f} seconds".format(csv_file.strip(), execution_time))
+
+        # Save the output to a CSV file
+        output_df = pd.DataFrame({'File': file_list['File Path'], 'Averaged RMSE': rmse_values, 'Standard Deviation of RMSE': std_dev_values})
+        output_df.to_csv(output_file_name, index=False)  # Save results to a separate output file
+
+        # Save execution time to a separate file
+        execution_times_df = pd.DataFrame({'list_csv': [csv_file.strip()], 'execution_time': [execution_time]})
+        execution_times_df.to_csv(f"{script_name}_{os.path.splitext(csv_file.strip())[0].replace('.', '_')}_execution_times.csv", index=False)
+
+    except Exception as e:
+        print("Error processing CSV file: {}. {}".format(csv_file.strip(), str(e)))
+
+# Use multiprocessing to process CSV files concurrently
+with Pool(processes=4) as pool:
+    pool.map(process_csv, list_csv)
+
+# Stop measuring the total execution time
+total_execution_end_time = time.time()
+
+# Calculate and print the total execution time for all CSV files
+total_execution_time = total_execution_end_time - total_execution_start_time
+print()
+print("Total execution time for all CSV files: {:.2f} seconds".format(total_execution_time))
+print()
---
+++ AdaBoost_bootstrapingx10.py
@@ -0,0 +1,129 @@
+import pandas as pd
+from sklearn.ensemble import AdaBoostRegressor
+from sklearn.metrics import mean_squared_error
+import math
+import os
+import time
+from sklearn.model_selection import train_test_split
+import numpy as np
+import multiprocessing
+
+# Funkcja wykonująca operacje na pojedynczym pliku
+def process_file(file_name):
+    try:
+        # Read the current file
+        data = pd.read_csv(file_name)
+
+        # Extract the features and target variable
+        X = data.iloc[:, 1:]  # Features (assuming they start from the second column)
+        y = data.iloc[:, 0]  # Target variable (first column)
+
+        # Check if there are enough data points for training
+        if len(data) <= 1:
+            raise ValueError("Insufficient data points in file: {}".format(file_name))
+
+        # Create an AdaBoost Regressor model
+        ada_regressor = AdaBoostRegressor()
+
+        # Perform fivefold bootstrapping
+        n_bootstraps = 10
+        rmse_values = []
+
+        for _ in range(n_bootstraps):
+            X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=np.random.randint(1, 1000))
+            ada_regressor.fit(X_train, y_train)
+            y_pred = ada_regressor.predict(X_test)
+            mse = mean_squared_error(y_test, y_pred)
+            rmse_values.append(math.sqrt(mse))
+
+        # Calculate the root mean squared error (RMSE) and standard deviation
+        rmse = np.mean(rmse_values)
+        std_dev_rmse = np.std(rmse_values)
+
+        return file_name, rmse, std_dev_rmse
+
+    except Exception as e:
+        print("Error processing file: {}. {}".format(file_name, str(e)))
+        return None
+
+# Clear the terminal screen
+# os.system('cls' if os.name == 'nt' else 'clear')
+
+# Read the CSV file containing the list of files
+list_csv_input = input("Enter the names of CSV files with lists of files (comma-separated): ")
+list_csv_files = [file.strip() for file in list_csv_input.split(',')]
+
+# Get the script name without the extension
+script_name = os.path.splitext(os.path.basename(__file__))[0]
+
+# Create a directory for output files if it doesn't exist
+output_dir = "output_files"
+os.makedirs(output_dir, exist_ok=True)
+
+# Create a list to store execution times
+execution_times = []
+
+# Iterate over the CSV files with lists of files
+for list_csv in list_csv_files:
+    # Remove whitespaces from the filename and add '_output.csv' as the output filename
+    output_filename = os.path.join(output_dir, f"{script_name}_{list_csv}_output.csv")
+
+    # Create empty lists to store RMSE and standard deviation values for each file
+    rmse_values = []
+    std_dev_values = []
+
+    # Start measuring the execution time for the current list
+    start_time = time.time()
+
+    try:
+        # Read the CSV file containing the list of files
+        file_list = pd.read_csv(list_csv)
+
+        # Parallel processing using multiprocessing
+        with multiprocessing.Pool(processes=4) as pool:
+            results = pool.map(process_file, file_list['File Path'])
+
+        # Stop measuring the execution time for the current list
+        end_time = time.time()
+
+        # Calculate the total execution time for the current list
+        execution_time = end_time - start_time
+
+        # Append execution time to the list
+        execution_times.append({'list_csv': list_csv, 'execution_time': execution_time})
+
+        # Print the file names with corresponding RMSE values and standard deviations for the current list
+        print()
+        print(f"RMSE values with Standard Deviations for datasets in {list_csv}:")
+        print()
+        for file_name, rmse, std_dev in results:
+            if file_name is not None:
+                # Extract the filename from the file path
+                filename = os.path.basename(file_name)
+                print("RMSE: {:.5f}  ----->  Standard Deviation of RMSE: {:.5f} ----->  File: {}".format(rmse, std_dev, filename))
+                rmse_values.append(rmse)
+                std_dev_values.append(std_dev)
+            else:
+                print("Error processing one or more files in the list.")
+
+        # Save the results to a CSV file
+        result_df = pd.DataFrame({
+            'File': file_list['File Path'],
+            'RMSE': rmse_values,
+            'Standard Deviation of RMSE': std_dev_values
+        })
+        result_df.to_csv(output_filename, index=False)
+
+        print("Total execution time for {}: {:.2f} seconds".format(list_csv, execution_time))
+        print()
+
+    except Exception as e:
+        print("Error processing CSV file list: {}. {}".format(list_csv, str(e)))
+
+# Create a DataFrame from the list of execution times
+execution_times_df = pd.DataFrame(execution_times)
+
+# Save the execution times DataFrame to a CSV file
+execution_times_df.to_csv(os.path.join(output_dir, f"{script_name}_execution_times.csv"), index=False)
+
+# End of the script
